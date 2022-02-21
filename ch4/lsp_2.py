7736850c17cc3b7a2b41c18c91aea1c90d22e67b
kks <drather@naver.com>
2022-02-22 00:15:41 +0900

    ch4 - SOILD, example of lsp, bad lsp example 2

---
+++ ch4/lsp_2.py
@@ -0,0 +1,75 @@
+class Event:
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+
+    @staticmethod
+    def meets_condition(event_data: dict):
+        return False
+
+
+class UnknownEvent(Event):
+    """
+     식별 불가한 이벤트
+    """
+
+
+class LoginEvent(Event):
+    """
+    로그인 사용자에 의한 이벤트
+    """
+    @staticmethod
+    def meets_condition(event_data: dict):
+        return event_data["before"]["session"] == 0 and event_data["after"]["session"] == 1
+
+
+class LogoutEvent(Event):
+    """
+    로그아웃 사용자에 의한 이벤트
+    """
+    @staticmethod
+    def meets_condition(event_data: dict):
+        return event_data["before"]["session"] == 1 and event_data["after"]["session"] == 0
+
+
+class TransactionEvent(Event):
+    @staticmethod
+    def meets_condition(event_data: dict):
+        return event_data["after"].get("transaction") is not None
+
+
+class BadEvent(Event):
+    def meets_condition(event_data: dict, override: bool) -> bool:
+        if override:
+            return True
+
+
+class SystemMonitor:
+    """
+    시스템에서 발생한 이벤트 분류
+    """
+    def __init__(self, event_data):
+        self.event_data = event_data
+
+    def identify_data(self):
+        for event_cls in Event.__subclasses__():
+            try:
+                if event_cls.meets_condition(self.event_data):
+                    return event_cls(self.event_data)
+            except KeyError:
+                continue
+
+        return UnknownEvent(self.event_data)
+
+
+if __name__ == '__main__':
+    l1 = SystemMonitor({"before": {"session": 0}, "after": {"session": 1}})
+    print(l1.identify_data().__class__.__name__)
+
+    l2 = SystemMonitor({"before": {"session": 1}, "after": {"session": 0}})
+    print(l2.identify_data().__class__.__name__)
+
+    l3 = SystemMonitor({"before": {"session": 1}, "after": {"session": 1}})
+    print(l3.identify_data().__class__.__name__)
+
+    l4 = SystemMonitor({"before": {"session": 1}, "after": {"session": 1, "transaction": 1}})
+    print(l4.identify_data().__class__.__name__)
