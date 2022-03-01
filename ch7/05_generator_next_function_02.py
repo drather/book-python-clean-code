f5182d1bae7d951bfcc13f953c30b0880f7c0cf3
kks <drather@naver.com>
2022-03-01 22:50:21 +0900

    ch7 - generator, next example 02

---
+++ ch7/05_generator_next_function_02.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    word = iter("hello")
+
+    print(next(word, "doesn't have a element"))
+    print(next(word, "doesn't have a element"))
+    print(next(word, "doesn't have a element"))
+    print(next(word, "doesn't have a element"))
+    print(next(word, "doesn't have a element"))
+    print(next(word, "doesn't have a element"))
