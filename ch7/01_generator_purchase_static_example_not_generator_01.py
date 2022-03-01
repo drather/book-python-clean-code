88d7543b104dd89911d147a366571b3b1c494a9f
kks <drather@naver.com>
2022-03-01 19:34:05 +0900

    ch7 - generator, add calculating purchases data with not generator

---
+++ ch7/01_generator_purchase_static_example_not_generator_01.py
@@ -0,0 +1,49 @@
+class PurchaseStats:
+    def __init__(self, purchases):
+        self.purchases = iter(purchases)
+        self.min_price: float = None
+        self.max_price: float = None
+        self._total_purchases_price: float = 0.0
+        self._total_purchases = 0
+        self._initialize()
+
+    def _initialize(self):
+        try:
+            first_value = next(self.purchases)
+
+        except:
+            raise ValueError("no values provided")
+
+        self.min_price = self.max_price = first_value
+        self._update_avg(first_value)
+
+    def process(self):
+        for purchase_value in self.purchases:
+            self._update_min(purchase_value)
+            self._update_max(purchase_value)
+            self._update_avg(purchase_value)
+
+        return self
+
+    def _update_min(self, new_value: float):
+        if new_value < self.min_price:
+            self.min_price = new_value
+
+    def _update_max(self, new_value: float):
+        if new_value > self.max_price:
+            self.max_price = new_value
+
+    @property
+    def avg_price(self):
+        return self._total_purchases_price / self._total_purchases
+
+    def _update_avg(self, new_value: float):
+        self._total_purchases_price += new_value
+        self._total_purchases += 1
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__} ({self.min_price})",
+            f"{self.max_price}, {self.avg_price}"
+        )
+
