e9134d3a9605bf59e80f4ae4962d2624f52c46d8
kks <drather@naver.com>
2022-03-04 16:07:30 +0900

    ch7 - unittest & refactoring, merge example, parametrized test - main code

---
+++ ch8/test_refactoring_08_unittest_example_03.py
@@ -0,0 +1,18 @@
+import unittest
+
+from ch8.test_refactoring_06_unittest_example_01 import MergeRequestStatus
+
+
+class AcceptanceThreshold:
+    def __init__(self, merge_request_context: dict) -> None:
+        self._context = merge_request_context
+
+    def status(self):
+        if self._context["downvotes"]:
+            return MergeRequestStatus.REJECTED
+        elif len(self._context["upvotes"]) >= 2:
+            return MergeRequestStatus.APPROVED
+
+        return MergeRequestStatus.PENDING
+
+
